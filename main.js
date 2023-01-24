ca9985c6079f2e2798aef9466fe5e33b917174e4
Sebastian Post <sebastianpost@protonmail.com>
2023-01-24 21:52:58 +0100

    Delete console.log

--- main.js
+++ main.js
@@ -4,7 +4,6 @@ const numbers6 = document.querySelector(".numberSix");
 const numbers12 = document.querySelector(".numberTwelve");
 const numbers9 = document.querySelector(".numberNine");
 const numbers3 = document.querySelector(".numberThree");
-console.log(numbers12)
 function btnPushed() {
     gsap.to([numbers6], { 
        duration: 5, x: 79.32, y: 20.31,
